5f8a0fe7396f66d6da3022668f9081108f4bead3
LorenzoAHenrique <lorenzoarrudahenrique@gmail.com>
2022-04-05 16:16:02 -0300

    last issue

--- get-quote.py
+++ get-quote.py
@@ -1,17 +1,19 @@
 import random
 def primary():
    #print("Keep it logically awesome.")
-
+   f = open("quotes.txt", "a")
+   f.write("\nInsert quote")
+   f.close()
    f = open("quotes.txt")
    quotes = f.readlines()
    f.close()
 
    last = len(quotes) - 1
    rnd = random.randint(0, last)
-   print(quotes[rnd])
+   print(quotes[rnd], end = '')
    last = len(quotes) - 1
    rnd = random.randint(0, last)
-   print(quotes[rnd])
+   print(quotes[rnd], end = '')
    last = len(quotes) - 1
    rnd = random.randint(0, last)
    print(quotes[rnd])
